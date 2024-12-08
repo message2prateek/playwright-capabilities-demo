c7408b2021c92939d65aa22df5c041c237eb66ed
Prateek Sharma <message2prateek@gmail.com>
2024-12-08 11:32:57 +1100

    Added test do showcase parameterisation

--- tests/parameterised-tests/simple-parameterised-test.spec.ts
+++ tests/parameterised-tests/simple-parameterised-test.spec.ts
@@ -1,19 +1,19 @@
 import test, { expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-  // ...
+    // ...
 });
 
 test.afterEach(async ({ page }) => {
-  // ...
+    // ...
 });
 
 [
-  { name: 'Alice', fullName: 'Hello, Alice!' },
-  { name: 'Bob', fullName: 'Hello, Bob!' },
-  { name: 'Charlie', fullName: 'Hello, Charlie!' },
+    { name: "Alice", fullName: "Hello, Alice!" },
+    { name: "Bob", fullName: "Hello, Bob!" },
+    { name: "Charlie", fullName: "Hello, Charlie!" },
 ].forEach(({ name, fullName }) => {
-  test(`testing with ${name}`, async ({ page }) => {
-    expect(fullName).toContain(name)
-  });
+    test(`testing with ${name}`, async ({ page }) => {
+        expect(fullName).toContain(name);
+    });
 });
